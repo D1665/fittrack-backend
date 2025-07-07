cd8fe9255feb22480ea21154027d5d289e665f2a
Dinesh Sirsiya <dineshsirsiya75@gmail.com>
2025-07-08 02:31:11 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./routes/User.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true })); // for form data
+
+app.use("/api/user/", UserRoutes);
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello developers from GFG",
+  });
+});
+
+const MONGODB_URI = "mongodb://127.0.0.1:27017/gfgDB";
+// const MONGODB_URI = "mongodb+srv://rahul:test123@cluster0.mongodb.net/gfgDB";
+
+export const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    await mongoose.connect(MONGODB_URI);
+    console.log("✓ MongoDB connected");
+  } catch (err) {
+    console.error("✗ MongoDB connection failed");
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(8008, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+startServer();
